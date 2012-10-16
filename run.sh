1e7200495a814aacd9ebd5da6529bea82f0b07f3
Andrew Turpin <aturpin@unimelb.edu.au>
2012-10-16 16:30:35 +1100

    Altered decision rule to be parameterised with rank of cell whose number of spikes is largest. For example, you can choose the fourth highest spiking cell as the decider.
    Also changed the search for threshold a little.

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#
+# Run for various values of DECISION_RANK 4
+#
+
+for i in 1 # 2 3 4 5 6 7 8 9 10
+do
+    cp model.h model.h.temp
+    sed 's/#define DECISION_RANK.*$/#define DECISION_RANK '"$i"'/' < model.h.temp > model.h
+    make
+    cat model.h
+    ./model x
+done
