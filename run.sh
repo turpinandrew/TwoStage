ab09401181482581a972fb739e661827d6f1bc83
Andrew Turpin <aturpin@unimelb.edu.au>
2012-10-16 16:34:02 +1100

    added run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,15 @@
 # Run for various values of DECISION_RANK 4
 #
 
-for i in 1 # 2 3 4 5 6 7 8 9 10
+for i in 1 2 3 4 5 6 7 8 9 10
 do
     cp model.h model.h.temp
     sed 's/#define DECISION_RANK.*$/#define DECISION_RANK '"$i"'/' < model.h.temp > model.h
     make
     cat model.h
+    ./model x
+    ./model x
+    ./model x
+    ./model x
     ./model x
 done
